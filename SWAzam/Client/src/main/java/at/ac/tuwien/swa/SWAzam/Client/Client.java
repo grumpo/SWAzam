dac3d52e19d2dbdbd8e0dd10182aafc99f2c971e
Karatekiwi <caitilyn@gmail.com>
2013-12-17 09:42:04 +0100

    starting work on client module

--- SWAzam/Client/src/main/java/at/ac/tuwien/swa/SWAzam/Client/Client.java
+++ SWAzam/Client/src/main/java/at/ac/tuwien/swa/SWAzam/Client/Client.java
@@ -4,6 +4,8 @@ import ac.at.tuwien.infosys.swa.audio.Fingerprint;
 import ac.at.tuwien.infosys.swa.audio.FingerprintSystem;
 import at.ac.tuwien.swa.SWAzam.Client.Client2PeerConnector.Client2PeerConnector;
 import at.ac.tuwien.swa.SWAzam.Client.Client2PeerConnector.FingerprintResult;
+import at.ac.tuwien.swa.SWAzam.Client.gui.LoginFrame;
+
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
@@ -32,15 +34,19 @@ public class Client {
     }
 
     public void run() {
+    	
+    	new LoginFrame();
+    	
+    	
         log.info("Client has been started an is running now...");
 
         // test identification
-        FingerprintSystem fingerprintSystem = new FingerprintSystem(44f);
+        /*FingerprintSystem fingerprintSystem = new FingerprintSystem(44f);
         byte[] fakeMp3 = new byte[23000];
         new Random().nextBytes(fakeMp3);
         Fingerprint fingerprint = fingerprintSystem.fingerprint(fakeMp3);
         FingerprintResult fingerPrintIdentificationResult = identify(fingerprint);
-        log.info("Fingerprint was identified to be: " + fingerPrintIdentificationResult.getResult());
+        log.info("Fingerprint was identified to be: " + fingerPrintIdentificationResult.getResult());*/
     }
 
     private FingerprintResult identify(Fingerprint fingerprint) {
