5a02f57d534c6ac0113152da2730e348ac7d2c8d
Markus Wolf <mawiwo@a1.net>
2013-12-17 09:56:06 +0100

    Added User Class

---
+++ SWAzam/Client/src/main/java/at/ac/tuwien/swa/SWAzam/Client/Controller/User.java
@@ -0,0 +1,27 @@
+package at.ac.tuwien.swa.SWAzam.Client.Controller;
+
+/**
+ * Created by markus on 17.12.13.
+ */
+public class User {
+    String username;
+    String password;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    @Override
+    public String toString(){
+        return username + ": " + password;
+    }
+}
