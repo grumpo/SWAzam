b1b9887e97923087a3fffe5a8f38913a62916577
Johannes Binder <j.binder.x@gmail.com>
2013-12-28 11:07:47 +0100

    connector updated

--- SWAzam/Peer/src/main/java/at/ac/tuwien/swa/SWAzam/Peer/Peer2ServerConnector/Peer2ServerConnector.java
+++ SWAzam/Peer/src/main/java/at/ac/tuwien/swa/SWAzam/Peer/Peer2ServerConnector/Peer2ServerConnector.java
@@ -4,4 +4,6 @@ import at.ac.tuwien.swa.SWAzam.Peer.Common.UserInformation;
 
 public interface Peer2ServerConnector {
     UserInformation validateUser(String user, String password) throws UnableToConnectToServerException;
+    boolean resolvedIdentification(String user, String password) throws UnableToConnectToServerException;
+    boolean requestedIdentification(String user, String password) throws UnableToConnectToServerException;
 }
